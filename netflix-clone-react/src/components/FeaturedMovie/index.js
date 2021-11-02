59eb597aba5757d78bb3137a5a8dc9b3bcc797c7
jefersonrgomes <jeferson.rgomes@hotmail.com>
2021-11-02 07:09:25 -0300

    update styles css

--- netflix-clone-react/src/components/FeaturedMovie/index.js
+++ netflix-clone-react/src/components/FeaturedMovie/index.js
@@ -1,20 +1,48 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import * as S from './styles'
 
 export function FeaturedMovie({ item }) {
+    console.log(item);
+
+    let firstDate = new Date(item.first_air_date);
+    //criando uma variavel para receber arrays
+    let genres = [];
+    //criando um laço for que percorrera todos itens dentro do arrai genres (*genero de filmes)
+    for (let i in item.genres) {
+        //para cada item encontrado, ira adicionar (.push) o valor de nome correspondente ao index do genero que esta passando pelo iterador no momento ('[i]')
+        genres.push(item.genres[i].name);
+    }
+
     return (
+
         <S.Container>
             <section className='featured'
-            style={{
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
-            }}>
-                {item.original_name}
-            
-            <div className='featured--vertical'>
+                style={{
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+                }}>
 
-            </div>
+                <div className='featured--vertical'>
+                    <div className='featured--horizontal'>
+                        <div className='featured--name'> {item.original_name} </div>
+                        <div className='featured--info'>
+                            <div className='featured--points'>{item.vote_average} Pontos</div>
+                            <div className='featured--year'>{firstDate.getFullYear()}</div>
+                            <div className='featured--seasons'>{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''} </div>
+                            <div className='featured--overview'>{item.overview}</div>
+                            <div className='featured--buttons'>
+                                <a href="/#" className='featured--watchButton'>Assistir</a>
+                                <a href="/#" className='featured--myListButton'>+ Minha Lista</a>
+                            </div>
+                            <div className='featured--genres'>
+                                <strong>Gêneros: </strong>
+                                {genres.join(', ')}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </section>
         </S.Container>
     )
--- netflix-clone-react/src/components/FeaturedMovie/styles.js
+++ netflix-clone-react/src/components/FeaturedMovie/styles.js
@@ -3,9 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
 
     .featured{
-        width: 100%;
         height: 100vh;
-      
         }
 
         .featured--vertical{
@@ -13,4 +11,87 @@ export const Container = styled.div`
             height: inherit;
             background: linear-gradient(to top, #111 10%, transparent 90%);
         }
+
+        .featured--horizontal{
+            width: inherit;
+            height: inherit;
+            background: linear-gradient(to right, #111 35%, transparent 75%);
+        }
+
+        .featured--name{
+            font-size: 60px;
+            font-weight: bold;
+            margin-left: 15px;
+
+        }
+
+        .featured--info{
+            font-size: 18px;
+            font-weight: bold;
+            margin-top: 15px;
+            margin-right: 15px;
+            margin-left: 15px;
+        }
+
+        .featured--points, .featured--year, .featured--seasons
+        {
+            display: inline-block;
+            margin-right: 15px;
+        }
+        .featured--points{
+            color: #46d369;
+        }
+
+        .featured--overview{
+            margin-top: 15px;
+            margin-bottom: 15px;
+            max-width: 40%;
+            color:#999;
+            font-weight: 400;
+            font-size: 20px;
+        }
+
+        .featured--buttons{
+            text-decoration: none;
+            color: white;
+            border: none;
+            margin-top: 15px;
+            margin-bottom: 15px;
+        }
+
+        .featured--watchButton, .featured--myListButton{
+            padding: 15px 25px;
+            margin-right: 15px;
+            display: inline-block;
+            border-radius: 10px;
+            font: bold;
+            font-size: 20px;
+            text-decoration: none;
+            transition: all ease 0.3s;
+            &:hover{
+                opacity: 0.7;
+            }
+
+        }
+
+        .featured--watchButton{
+            background-color: white;
+            color: black;
+        }
+
+        .featured--myListButton{
+            background-color: #222;
+            color: white;
+        }
+
+        .featured--genres{
+            color:#999;
+            font-weight: 400;
+            font-size: 20px;
+            margin-right: 15px; 
+        }
+
+        
+
+
 `;
